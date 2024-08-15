e7667e28f0320fa6bf76eea6677404f086432f0f
Thomas Brears <fcx9443@autuni.ac.nz>
2024-08-15 12:18:34 +1200

    final changes before hosting

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb+srv://thomas:comp602@cluster0.rffxuzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.rffxuzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     await client.connect();
     db = client.db('react-blog-db');
     cb();
